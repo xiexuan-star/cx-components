6b86f3d3487602246f664d57906f37a24fa611d6
Philip <Philip@chxian.com>
2021-07-15 19:19:28 +0800

    feature 删去dayjs引用

--- chx-ui/src/utils/index.ts
+++ chx-ui/src/utils/index.ts
@@ -1,12 +1,7 @@
 import { AnyObject } from '../types'
 import { isObject } from './is'
-import dayjs from 'dayjs'
 
 export * from './is'
-
-export const parseTime = (time: string, format = 'YYYY-MM-DD HH:mm') => {
-  return dayjs(time).format(format)
-}
 
 export function omit<T extends AnyObject, K extends keyof T>(target: T, keys: K[]) {
   if (!isObject(target)) return target
