98cb2d39d3c905a66dbc9446023f174ac4721239
Philip <Philip@chxian.com>
2022-02-14 15:28:55 +0800

    perf: 删去dist

--- chx-ui/dist/lib/cx-tab/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { SFCWithInstall } from '../../types/interface';
-declare const _CX_TAB: SFCWithInstall<import("vue").DefineComponent<{}, {}, any, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{} & {}>, {}>>;
-export default _CX_TAB;
--- chx-ui/dist/lib/cx-tab/type.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type CxTabOption = (NameWithId & Partial<{
-    badgeKey: string;
-    hide: boolean;
-    unit: string;
-}>);
