adf406c00cd7ec7350fa20043c2fef4765724710
nicole <nicole@chxian.com>
2022-05-19 12:13:35 +0800

    feat: 表格分页pageCapacity切换时本地存储

--- chx-ui/src/lib/cx-table/hooks/useCxPagination.ts
+++ chx-ui/src/lib/cx-table/hooks/useCxPagination.ts
@@ -2,12 +2,12 @@ import { localStore } from 'chx-utils';
 import { reactive } from 'vue';
 import { PaginationModel } from '../types';
 
-const PAGE_CAPACITY = '_page_capacity';
+export const PAGE_CAPACITY = '_page_capacity';
 
 export function useCxPagination(useCache = true) {
   return reactive<PaginationModel>({
     currentPage: 1,
-    pageCapacity: useCache ? localStore.get(PAGE_CAPACITY) || 10 : 10,
+    pageCapacity: useCache ? (localStore.get(PAGE_CAPACITY) || 10) : 10,
     pageSizes: [10, 20, 50],
     total: 0
   });
