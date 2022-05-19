ad8f0fc3ad99b59c7f11fb982bd3d1a53c32b85a
nicole <nicole@chxian.com>
2022-05-19 11:53:15 +0800

    feat: useCxPagination pageCapacity 默认记住上一次选择

--- chx-ui/src/lib/cx-table/hooks/useCxPagination.ts
+++ chx-ui/src/lib/cx-table/hooks/useCxPagination.ts
@@ -1,10 +1,13 @@
+import { localStore } from 'chx-utils';
 import { reactive } from 'vue';
 import { PaginationModel } from '../types';
 
-export function useCxPagination() {
+const PAGE_CAPACITY = '_page_capacity';
+
+export function useCxPagination(useCache = true) {
   return reactive<PaginationModel>({
     currentPage: 1,
-    pageCapacity: 10,
+    pageCapacity: useCache ? localStore.get(PAGE_CAPACITY) || 10 : 10,
     pageSizes: [10, 20, 50],
     total: 0
   });
